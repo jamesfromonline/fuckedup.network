bf1816cdf5a2da71b492b0c538a8d877710eaed5
jamesfromonline <jtwebco@gmail.com>
2023-01-28 17:43:55 -0500

    loader fix

--- ui/Layout.tsx
+++ ui/Layout.tsx
@@ -6,9 +6,9 @@ import { useState } from "react"
 const NavBar = dynamic(() => import("./NavBar"))
 
 const Loader = () => (
-  <div className="w-full min-h-[70vh] flex-auto flex flex-col items-center justify-center z-10 relative">
+  <div className="w-full min-h-[70vh] flex-auto flex flex-col items-center justify-center z-10 relative overflow-hidden">
     <div className="w-28 h-28 relative rounded-full overflow-hidden shadow-md">
-      <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-r from-primary to-secondary animate-spin z-0" />
+      <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-r from-primary to-secondary animate-spin z-0 rounded-full" />
       <div className="w-28 h-28 bg-black grid place-items-center text-md leading-none font-bold text-primary relative z-10 scale-[0.9] rounded-full text-center">
         fucking <br /> loading
       </div>
